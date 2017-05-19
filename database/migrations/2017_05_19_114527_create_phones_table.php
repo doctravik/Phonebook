c8023ddf277db223a7951a1c987e54adb90084cf
doctravik <doctravik@gmail.com>
2017-05-19 15:41:19 +0300

    Config migrations

--- database/migrations/2017_05_19_114527_create_phones_table.php
+++ database/migrations/2017_05_19_114527_create_phones_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePhonesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('phones', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->index();
+            $table->integer('user_id')->unsigned();
+            $table->string('number', 11)->unique();
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('phones');
     }
 }
